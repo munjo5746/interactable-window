13793671ad2f43dc033e9a9b279c340d981eba93
Edward Chung <munjo5746@gmail.com>
2021-03-28 16:33:15 -0400

    basic window dragging

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+const App: React.FC = () => {
+    const isDragging = React.useRef<boolean>(false);
+    const windowRef = React.useRef<HTMLDivElement>(null);
+    React.useEffect(() => {
+        const mousedown = (e: MouseEvent) => {
+            isDragging.current = true;
+        };
+
+        const mousemove = (e: MouseEvent) => {
+            if (!windowRef.current || !isDragging.current) return;
+
+            windowRef.current.style.top = `${e.clientY}px`;
+            windowRef.current.style.left = `${e.clientX}px`;
+        };
+
+        const mouseup = (e: MouseEvent) => {
+            isDragging.current = false;
+            console.log('mouse up');
+        };
+
+        window.addEventListener('mousedown', mousedown);
+
+        window.addEventListener('mousemove', mousemove);
+
+        window.addEventListener('mouseup', mouseup);
+
+        return () => {
+            window.removeEventListener('mousedown', mousedown);
+            window.removeEventListener('click', mousemove);
+            window.removeEventListener('mouseup', mouseup);
+        };
+    }, []);
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
+        <div className="root">
+            <div
+                ref={windowRef}
+                className="window"
+                style={{ width: '500px', height: '300px' }}
+            >
+                <div
+                    className="draggable-section"
+                    style={{
+                        width: '100%',
+                        height: '50px',
+                        background: 'gray',
+                    }}
                 >
-                    Learn React
-                </a>
-            </header>
+                    draggable
+                </div>
+                <div className="content">content</div>
+            </div>
         </div>
     );
-}
+};
 
 export default App;
