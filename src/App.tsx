c1dd766cd021606b34c4873fc6c771bff6e35d70
Edward Chung <munjo5746@gmail.com>
2021-03-28 18:08:42 -0400

    added logic to determine if dragging should start in on mouse click event

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,30 @@ const App: React.FC = () => {
     const windowRef = React.useRef<HTMLDivElement>(null);
     React.useEffect(() => {
         const mousedown = (e: MouseEvent) => {
+            // Determin if window dragging started
+            // 1. Check if clicked on window element
+            // 2. Check if dragging area clicked
+
+            const target = e.target as HTMLDivElement;
+            const possibleWindowDiv = target.parentElement;
+            const { type } = possibleWindowDiv?.dataset || {
+                id: possibleWindowDiv?.getAttribute('type') || 'none',
+            };
+            if (type !== 'window') return;
+
+            const { type: possibleDraggableDivType } = target.dataset;
+            if (possibleDraggableDivType !== 'draggable') return;
+
             isDragging.current = true;
         };
 
         const mousemove = (e: MouseEvent) => {
             if (!windowRef.current || !isDragging.current) return;
 
-            windowRef.current.style.top = `${e.clientY}px`;
-            windowRef.current.style.left = `${e.clientX}px`;
+            const window = windowRef.current as HTMLDivElement;
+
+            window.style.top = `${e.clientY}px`;
+            window.style.left = `${e.clientX}px`;
         };
 
         const mouseup = (e: MouseEvent) => {
@@ -37,10 +53,14 @@ const App: React.FC = () => {
         <div className="root">
             <div
                 ref={windowRef}
+                data-type={'window'}
+                data-id={'window-1'}
                 className="window"
                 style={{ width: '500px', height: '300px' }}
             >
                 <div
+                    data-type={'draggable'}
+                    data-id={`window-1-draggable`}
                     className="draggable-section"
                     style={{
                         width: '100%',
